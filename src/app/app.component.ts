97d2935f42b1f7cb1edab74aa2d10d1c933e78fc
shiv159 <zaynster007@gmail.com>
2024-04-09 20:01:59 +0530

    basic angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './comps/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ang17';
+  parent_data = 'Parent data dynamically passed to child component';
+  child_data = '';
+  getChildData(data: string) {
+    console.log('Data received from child:', data);
+    this.child_data = data; 
+  }
 }
---
+++ src/app/comps/user-profile/user-profile.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input,Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+  name='John Doe';
+  age=25;
+  btnDisabled=false;
+@Output() childData=new EventEmitter<string>();
+@Input({alias:"parent_data"}) parent_data:string="";
+
+
+  btnClick( e:Event){
+    console.log('Button clicked');
+    const value=(e.target as HTMLInputElement).value;
+    console.log('Button value:',value);
+    
+  }
+
+  users=[ 
+    {name:"Shivam", age:25},
+    {name:"Ankur", age:60},
+    {name:"Shraddha", age:24},
+    {name:"Ap", age:50}
+  ];
+
+  sendDataToParent(){
+    console.log('Data sent to parent:',this.name);
+    this.childData.emit(this.name);
+  }
+
+
+}
